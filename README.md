f76d053542b1b628ed8b34522f417d618b1b1973
theycallmemuck <otmuck@gmail.com>
2024-03-22 18:08:42 +0300

    add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mobile app assistent for playing table games with your friends
+
+```sh
+npm i
+
+npx expo start
+
+//TODO: build in progress
+```
--- src/widgets/PLayersResults/PlayersResults.tsx
+++ src/widgets/PLayersResults/PlayersResults.tsx
@@ -1,15 +1,22 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { FlatList, View } from "react-native";
+import { FlatList, View, Text, StyleSheet } from "react-native";
 import { RootStackRouteParams } from "../../screens/routes";
 
 const PlayersResults = () => {
-  const { params } = useRoute<RouteProp<RootStackRouteParams, "GameResults">>;
-  const {} = params;
+  const { params } = useRoute<RouteProp<RootStackRouteParams, "GameResults">>();
+  const { gameTime, players } = params;
+
   return (
     <View>
+      <Text style={styles.timeText}>{}</Text>
       <FlatList data={[]} renderItem={() => <View />} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+  timeText: {},
+});
 
 export default PlayersResults;
